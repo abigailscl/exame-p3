8db8d88ec5676f5420e874a6085ca17dfc33fbeb
Abigail Cabascango <abigail.cabascango@ioet.com>
2023-03-09 23:03:55 -0500

    add kong config

---
+++ Banquito-Exam-Kong/init.js
@@ -0,0 +1,11 @@
+db = db.getSiblingDB('branch');
+db.createCollection('branches');
+
+var brancheColl = db.getCollection('branches');
+
+brancheColl.insertMany([
+    {"code": "001", "name": "Guayaquil"},
+	{"code": "002", "name": "Quito"},
+	{"code": "003", "name": "Cuenca"},
+	{"code": "004", "name": "Ambato"}
+]);
